8fa4ca730e0de8444d47d4fd191341ff9e7600c0
Derek Chen-Becker <derek@simpleenergy.com>
2014-10-01 14:33:15 -0600

    Big refactor to bring kafka packaging up-to-speed
    * Switched to latest (0.8.1.1) version of Kafka
    * Fixed build script to call gradle (Kafka 0.8 no longer uses sbt)
    * Refactored build script to properly apply upstart and default files
    * Fixed upstart script to directly run Kafka instead of trying to re-use
      Kafka's included run scripts. This is a big deal because the kafka run
      scripts assume everything will run out of the packaging dir, which is
      not what you want on a Debian system

--- build_kafka.sh
+++ build_kafka.sh
@@ -2,7 +2,8 @@
 set -e
 set -u
 name=kafka
-version=0.7.2-incubating
+version=0.8.1.1
+scalaVersion=2.10.4
 description="Apache Kafka is a distributed publish-subscribe messaging system."
 url="https://kafka.apache.org/"
 arch="all"
@@ -10,7 +11,7 @@ section="misc"
 license="Apache Software License 2.0"
 package_version="-1"
 src_package="kafka-${version}-src.tgz"
-download_url="http://mirrors.sonic.net/apache/incubator/kafka/kafka-${version}/${src_package}"
+download_url="http://mirrors.sonic.net/apache/kafka/${version}/${src_package}"
 origdir="$(pwd)"
 
 #_ MAIN _#
@@ -22,21 +23,19 @@ mkdir -p tmp && pushd tmp
 rm -rf kafka
 mkdir -p kafka
 cd kafka
-mkdir -p build/usr/lib/kafka
+mkdir -p build/usr/lib/kafka/{bin,libs,config}
 mkdir -p build/etc/default
 mkdir -p build/etc/init
 mkdir -p build/etc/kafka
 mkdir -p build/var/log/kafka
 
-cp ${origdir}/kafka-broker.default build/etc/default/kafka-broker
-cp ${origdir}/kafka-broker.upstart.conf build/etc/init/kafka-broker.conf
-
 tar zxf ${origdir}/${src_package}
 cd kafka-${version}-src
-./sbt update
-./sbt package
-mv config/log4j.properties config/server.properties ../build/etc/kafka
-mv * ../build/usr/lib/kafka
+./gradlew -PscalaVersion=${scalaVersion} jar
+cp -v ${origdir}/artifacts/log4j.properties config/server.properties ../build/etc/kafka/
+cp -v bin/kafka*.sh ../build/usr/lib/kafka/bin/
+cp -v core/build/libs/* core/build/dependant-libs-${scalaVersion}/* ../build/usr/lib/kafka/libs/
+
 cd ../build
 
 fpm -t deb \
@@ -48,9 +47,12 @@ fpm -t deb \
     --category ${section} \
     --vendor "" \
     --license "${license}" \
-    -m "${USER}@localhost" \
+    -m "${MAINTAINER:-$USER@localhost}" \
     --prefix=/ \
     -s dir \
+    -d java7-runtime \
+    --deb-default ${origdir}/artifacts/default/kafka-broker \
+    --deb-upstart ${origdir}/artifacts/upstart/kafka-broker \
     -- .
 mv kafka*.deb ${origdir}
 popd
