97c409a9b80e01c4cd300bb465be7b813d7abc9f
Taylor Brockman <brockman@bamboo.poweranalytics.com>
2015-03-20 09:59:41 -0400

    Included a Ubuntu 14.04.* LTS compatible init.d script. Added a binary distribution script and updated the souce to use the gradle build.

--- dist_kafka.sh
+++ dist_kafka.sh
@@ -7,21 +7,20 @@ set -u
 name=kafka
 version=0.8.2.1
 scala_version=2.10
+package_version="-3"
 description="Apache Kafka is a distributed publish-subscribe messaging system."
 url="https://kafka.apache.org/"
 arch="all"
 section="misc"
 license="Apache Software License 2.0"
-package_version="-2"
 bin_package="kafka_${scala_version}-${version}.tgz"
-src_package="kafka-${version}-src.tgz"
-src_download_url="http://mirror.sdunix.com/apache/kafka/${version}/${src_package}"
+bin_download_url="http://mirror.sdunix.com/apache/kafka/${version}/${bin_package}"
 origdir="$(pwd)"
 
 #_ MAIN _#
 rm -rf ${name}*.deb
-if [[ ! -f "${src_package}" ]]; then
-  wget -c ${src_download_url}
+if [[ ! -f "${bin_package}" ]]; then
+  wget -c ${bin_download_url}
 fi
 mkdir -p tmp && pushd tmp
 rm -rf kafka
@@ -39,24 +38,14 @@ cp ${origdir}/kafka-broker.default build/etc/default/kafka-broker
 cp ${origdir}/kafka-broker.upstart.conf build/etc/init/kafka-broker.conf
 cp ${origdir}/kafka-broker.init.d build/etc/init.d/kafka
 
-# This code line uses the src packages
-tar zxf ${origdir}/${src_package}
-cd kafka-${version}-src
-# updated to gradle build as per Kafka README
-gradle
-./gradlew releaseTarGz
-cp ./core/build/distributions/${bin_package} ${origdir}/${bin_package}
-cd ..
-
-# Now resume regular binary exploding
 # Updated to use the Binary package
 rm -rf kafka_${scala_version}-${version}
 tar zxf ${origdir}/${bin_package}
 cd kafka_${scala_version}-${version}
+# mv config/log4j.properties config/server.properties ../build/etc/kafka
 cp config/server.properties ../build/etc/kafka
 mv * ../build/usr/lib/kafka
 cd ../build
-
 
 fpm -t deb \
     -n ${name} \
